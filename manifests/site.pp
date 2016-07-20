ff9b994f14afaf1f9ee130c4fa1e8f03a63862fc
skumar36 <sunilkumar20@gmail.com>
2016-07-20 22:19:33 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,7 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 }
-exec { 'cowsay 'Welcome to ${::fqdn}!' > /etc/motd':
+exec { 'create  /etc/motd':
 path  => '/usr/local/bin',
+command  => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
 }
