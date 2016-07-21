1bf636eba6f38d0400ca62a68cc4f5aa75b2f594
skumar36 <sunilkumar20@gmail.com>
2016-07-21 23:14:31 +0530

    Update init.pp

--- site/nginxtest/manifests/init.pp
+++ site/nginxtest/manifests/init.pp
@@ -1,1 +1,18 @@
-123
+class nginxtest {
+
+  File {
+    owner  =>  'apache',
+    group  =>  'apache',
+    mode   =>  '0644',
+    }
+    file { 'www':
+    ensure  => directory,
+    path  => '/var/www/
+    }
+    
+    file { '/var/www/file.conf':
+    content  =>  'testing 123',
+    ensure  => file,
+    }
+    
+    }
