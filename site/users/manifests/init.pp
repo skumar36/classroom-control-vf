b0cf2ff26c7c6d6e4fc46159cb8879427ae6935b
skumar36 <sunilkumar20@gmail.com>
2016-07-22 03:51:58 +0530

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,7 +1,10 @@
-class users {
-
-user { 'fundamentals':
-ensure => present,
+class users::admins {
+users::managed_user { 'sk123': }
+users::managed_user { 'sk1234':
+group => 'staff',
 }
 
+group { 'staff':
+ensure => present,
+}
 }
